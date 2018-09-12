51f2788514b53e9da90ef531b3c8baad3b81d86d
Cherian Porter <cherianbelun@gmail.com>
2018-09-12 03:07:08 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your code in this file!
+function scuberGreetingForFeet (feet) {
+ if (feet <= 400) {return 'This one is on me!'}
+}
+
+function scuberGreetingForFeet (feet) {
+ if (feet <= 400) {return 'This one is on me!'}
+}
+
+
+function ternaryCheckCity (city) {
+  
+}
+
+function switchOnCharmFromTip (tip) {
+  // switch (tip) {
+  //   case "generous" :
+  //     return "Thank you so much.",
+  //     // case secondCondition:
+  //     //   return results,
+  // }
+
+}
